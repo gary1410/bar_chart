c2f7d938ca279e8a3ba67f47bee14050968b413b
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-11 13:00:00 +0100

    Tests models   - This commit focuses on two points of testing.   1)  I'm testing the average cost per megabit in the module   2)  I'm then testing the ben_vs_avgcost method in the school.rb file

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,18 +2,13 @@ FactoryGirl.define do
 		factory :organization do
 			name 'Cascade Middle School'
 			ben  '114619'
-			phones    { [FactoryGirl.build(:phone1), FactoryGirl.build(:phone2)] }
+			purchases    { [FactoryGirl.build(:purchase), FactoryGirl.build(:purchase)] }
+		end
 
-	  factory :phone1, :class => :phone do
+	  factory :purchase, :class => :purchase do
 	    bandwidth   1000
-	    measures    "t"
+	    measure    "t"
 	    cost				90
 	  end
 
-	  factory :phone2, :class => :phone do
-	    bandwidth   100
-	    measures    "k"
-	    cost				50
-	  end
-	end
 end
