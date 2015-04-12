1e9a913e0867099a21653f828d1243633ecaa195
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-11 20:17:19 -0700

    made a calculations module and wrapped all calculations pertaining the schools in there

---
+++ app/models/concerns/calculations.rb
@@ -0,0 +1,40 @@
+module Calculations
+
+	def average_cost_per_megabit 
+		sum_of_purchases / total_number_of_purchases
+	end
+
+	def purchases
+		if School 
+			school_purchases
+		elsif Organization
+			purchases
+		end
+	end
+
+	def sum_of_purchases
+		sum = purchases.map { |purchase| purchase.purchase_price }.reduce(:+)
+	end
+
+	def total_number_of_purchases
+		total_purchase = purchases.count
+	end
+
+	def purchase_price
+		cost/bandwidth_in_mb
+	end
+
+	def bandwidth_in_mb
+		case measure
+		when "k" 
+			bandwidth.to_f / 1000
+		when "m"
+			bandwidth.to_f
+		when "g"
+			bandwidth.to_f * 1000
+		when "t"
+			bandwidth.to_f * 1000 * 1000
+		end
+	end
+
+end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,13 +1,8 @@
 class Organization
 	include Mongoid::Document
+	include Calculations
 	field :name, :type => String
 	field :ben, :type => String
 	embeds_many :purchases
-
-	def average_cost_per_megabit
-		sum_of_purchases = purchases.map { |purchase| purchase.purchase_price }.reduce(:+)
-		total_purchase = purchases.count
-		sum_of_purchases/total_purchase
-	end
 
 end
