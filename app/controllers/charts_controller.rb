d0b82f67914c2e084710966e0e0863b0a2dbccf0
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-11 20:28:55 -0700

    changes naming of method to be ben_vs_avg_cost

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,7 @@
 class ChartsController < ApplicationController
+
 	def show
-		@school_chart = School.build_chart
+		@school_chart = School.ben_vs_avg_cost
 	end
+
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,9 +1,9 @@
 class Purchase
 	include Mongoid::Document
+	include Calculations
 	field :bandwidth, :type => Integer
 	field :measure, :type => String
 	field :cost, :type => Integer
 	embedded_in :organization
-	include Calculations
 
 end
