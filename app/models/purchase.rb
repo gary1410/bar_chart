175173b8efc22f62291af868cd86831d3b53b408
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-11 20:17:19 -0700

    made a calculations module and wrapped all calculations pertaining the schools in there

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,22 +4,6 @@ class Purchase
 	field :measure, :type => String
 	field :cost, :type => Integer
 	embedded_in :organization
-
-	def purchase_price
-		cost/bandwidth_in_mb
-	end
-
-	def bandwidth_in_mb
-			case measure
-				when "k" 
-					bandwidth.to_f / 1000
-				when "m"
-					bandwidth.to_f
-				when "g"
-					bandwidth.to_f * 1000
-				when "t"
-					bandwidth.to_f * 1000 * 1000
-				end
-	end
+	include Calculations
 
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,5 @@
 class School < ActiveRecord::Base
+	include Calculations
 
 	has_many :school_purchases, primary_key: 'ben', foreign_key: 'ben'
 
@@ -7,18 +8,5 @@ class School < ActiveRecord::Base
 		ben_and_avg_cost = all_schools.map { |school_obj| [school_obj.ben, school_obj.average_cost_per_megabit]}
 	end
 
-#calcuates_average_cost_per_megabit
-
-	def average_cost_per_megabit 
-		sum_of_purchases / total_number_of_purchases
-	end
-
-	def sum_of_purchases
-		sum = school_purchases.map { |purchase| purchase.purchase_price }.reduce(:+)
-	end
-
-	def total_number_of_purchases
-		total_purchase = school_purchases.count
-	end
 
 end
