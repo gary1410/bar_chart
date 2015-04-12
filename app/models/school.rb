e26273c656aa1b85b6ca0bd79edf12f5acacab3b
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-11 20:28:55 -0700

    changes naming of method to be ben_vs_avg_cost

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,12 +1,10 @@
 class School < ActiveRecord::Base
 	include Calculations
-
 	has_many :school_purchases, primary_key: 'ben', foreign_key: 'ben'
 
-	def self.build_chart
+	def self.ben_vs_avg_cost
 		all_schools = School.all + Organization.all
 		ben_and_avg_cost = all_schools.map { |school_obj| [school_obj.ben, school_obj.average_cost_per_megabit]}
 	end
-
 
 end
